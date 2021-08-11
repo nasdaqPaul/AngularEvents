dba410e0a57e5233479b89918920f53c71743b48
paulNasdaq <nasdaqpaul@gmail.com>
2021-08-11 08:49:51 +0300

    Done event listing events

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
 import {NavbarComponent} from "./navbar/navbar.component";
+import {EventListComponent} from "./event-list/event-list.component"
+import {EventThumbnailComponent} from "./event-thumbnail/event-thumbnail.component";
+import {EventsService} from "./services/events.service";
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    EventListComponent,
+    EventThumbnailComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    EventsService
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ src/app/event-list/event-list.component.ts
@@ -0,0 +1,17 @@
+import {Component, OnInit} from "@angular/core";
+import {EventsService} from "../services/events.service";
+
+@Component({
+  selector: 'event-list',
+  templateUrl: 'event-list.component.html'
+})
+export class EventListComponent implements OnInit{
+  events: any;
+
+  constructor(private eventsService: EventsService) {
+  }
+
+  ngOnInit() {
+    this.events = this.eventsService.getEvents();
+  }
+}
